dc62f3bbd4837a9cc7cee80130292a398f749f48
regentgaming <youmeeeeee6@gmail.com>
2024-10-03 01:10:01 -0400

    Added SDL2 and configued it to run with a test window

--- main.cpp
+++ main.cpp
@@ -1,3 +1,43 @@
-int main() {
-    return 0;
+#include <iostream>
+#include "src/include/SDL2/SDL.h"
+
+int main(int argc, char *argv[]) {
+    const int WIDTH = 800, HEIGHT = 600;
+    SDL_Init(SDL_INIT_EVERYTHING);
+
+    SDL_Window *window = SDL_CreateWindow("Test window", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WIDTH, HEIGHT, SDL_WINDOW_ALLOW_HIGHDPI  | SDL_WINDOW_RESIZABLE);
+    
+    SDL_Event windowEvent;
+
+    if (!window) {
+        std::cout << "Failed to create window\n";
+        std::cout << "SDL2 Error: " << SDL_GetError() << "\n";
+        return EXIT_FAILURE;
+    }
+
+    SDL_Renderer *renderer = SDL_CreateRenderer(window,-1, SDL_RENDERER_PRESENTVSYNC);
+    if(!renderer)
+    {
+        std::cout << "Failed to create renderer\n";
+        std::cout << "SDL2 Error: " << SDL_GetError() << "\n";
+        return EXIT_FAILURE;
+    }
+
+    SDL_SetRenderDrawColor(renderer,255,255,255,255);
+    SDL_RenderClear(renderer);
+    SDL_RenderPresent(renderer);
+    SDL_RenderPresent(renderer);
+    bool keep_window_open = true;
+    while (keep_window_open) {
+        while (SDL_PollEvent(&windowEvent) > 0) {
+            switch(windowEvent.type) {
+                case SDL_QUIT:
+                keep_window_open = false;
+                    break;
+            }
+        }
+    }
+
+    return EXIT_SUCCESS;
+
 }
