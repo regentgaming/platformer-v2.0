cc24cfc38f7ad4fa4fd7d1e3a99d417ad50e6abd
regentgaming <youmeeeeee6@gmail.com>
2024-11-01 20:33:04 -0400

    Moved Vector2D to its own file, and made the beginnings of BoundingBox

--- include/Platformer-V2.0/physics.hpp
+++ include/Platformer-V2.0/physics.hpp
@@ -1,51 +1,23 @@
 #include "icollidable.hpp"
+#include "util.hpp"
 #include <vector>
 
 class Physics {
-    static std::vector<ICollidable> statics;
-    static std::vector<ICollidable> dynamics;
+    static std::vector<ICollidable*> statics;
+    static std::vector<ICollidable*> dynamics;
     //Dummy virtual function to make the class abstract
     virtual void makingAbstract() = 0;
 
     public:
-        struct Vector2D {
-            private:
-                double x;
-                double y;
-            public:
-                Vector2D(double x, double y) {
-                    this->x = x;
-                    this->y = y;
-                }
-                
-                /**
-                 * @brief Returns the x value
-                 * @return the x value
-                 */
-                double getX() {return x;}
-                /**
-                 * @brief Returns the y value
-                 * @return the y value
-                 */
-                double getY() {return y;}
-
-                Vector2D operator+(Vector2D const& other) {
-                    Vector2D ret = Vector2D(x + other.x, y + other.y);
-                }
-        };
-
-
-
-
         /**
          * @brief Adds an ICollidable object to the list of static objects.
          * If the list has nothing in it, instantiates the list
          * @param object The object to be added
          * @return None
          */
-        static void addStatic(ICollidable object) {
+        static void addStatic(ICollidable* object) {
             if (&statics == nullptr) {
-                statics = std::vector<ICollidable>();
+                statics = std::vector<ICollidable*>();
             }
             statics.push_back(object);
         }
@@ -56,10 +28,12 @@ class Physics {
          * @param object The object to be added
          * @return None
          */
-        static void addDynamic(ICollidable object) {
+        static void addDynamic(ICollidable* object) {
             if (&dynamics == nullptr) {
-                dynamics = std::vector<ICollidable>();
+                dynamics = std::vector<ICollidable*>();
             }
             dynamics.push_back(object);
         }
+
+        static void detectCollision();
 };
---
+++ include/Platformer-V2.0/util.hpp
@@ -0,0 +1,40 @@
+class Vector2D {
+            private:
+                double x;
+                double y;
+            public:
+                //this is the thing
+                Vector2D(double x_p, double y_p) : x(x_p), y(y_p) {}
+                Vector2D() = default;
+                /**
+                 * @brief Returns the x value
+                 * @return the x value
+                 */
+                double getX() {return x;}
+                /**
+                 * @brief Returns the y value
+                 * @return the y value
+                 */
+                double getY() {return y;}
+
+                Vector2D operator+(Vector2D const& other) {
+                    Vector2D ret = Vector2D(x + other.x, y + other.y);
+                }
+};
+
+class BoundingBox {
+    private:
+        Vector2D UL;
+        Vector2D LR;
+    public:
+        BoundingBox(double x, double y, double height, double width) {
+            UL = Vector2D(x,y);
+            LR = Vector2D(x+width,y+height);
+        }
+
+        BoundingBox() = default;
+
+        bool isIntersecting(BoundingBox other) {
+            return (LR.getX() > other.UL.getX() && other.LR.getX() > UL.getX()) && (LR.getY() > other.UL.getY() && other.LR.getY() > UL.getY());
+        }
+};
